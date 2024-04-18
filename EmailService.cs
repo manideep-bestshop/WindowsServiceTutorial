aa14d7c05448fb3f22e4f41053cd3070b6641815
manideep-bestshop <nmanideep1015@gmail.com>
2024-04-18 21:11:56 +0530

    Windows service added to send mail

---
+++ EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTutorials
+{
+    internal class EmailService
+    {
+        internal static void SendEmail(string message)
+        {
+            string fromMail = "nmanideep1015@gmail.com";
+            string fromPassword = "iwzfhxfrmjahomcu";
+
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(fromMail);
+            msg.Subject = message;
+            msg.To.Add(new MailAddress("nmanideep1015@gmail.com"));
+            msg.Body = "Service Related";
+            msg.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(msg);
+        }
+
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using System.Xml.Linq;
+
+namespace WindowsServiceTutorials
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer=new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string message = "Service started ";
+            EmailService.SendEmail(message);
+        }
+
+      
+        protected override void OnStop()
+        {
+            string message = "Service stopped ";
+            EmailService.SendEmail(message);
+        }
+        
+    }
+}
